43e6bb9e9181847993ed88d35bdb290826b6472d
Chazz Michaels <michaelchasedev@gmail.com>
2018-10-18 16:34:47 -0400

    add some default info to state that will be updated by app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,29 @@ import YourWorkout from './components/yourWorkout';
 import './App.css';
 
 class App extends Component {
+  state = {
+    exerciseList: [
+      {id: 1, type: "compound", mg: "legs", name: "BB Squat"},
+      {id: 2, type: "compound", mg: "legs", name: "Leg Press"},
+      {id: 3, type: "compound", mg: "back", name: "BB Deadlift"},
+      {id: 4, type: "compound", mg: "back", name: "Seated Cable Row"},
+      {id: 5, type: "compound", mg: "chest", name: "BB Bench Press"},
+      {id: 6, type: "compound", mg: "chest", name: "DB Chest Fly"},
+      {id: 7, type: "compound", mg: "shoulders", name: "BB Overhead Press"},
+      {id: 8, type: "compound", mg: "shoulders", name: "DB Lateral Raise"},
+      {id: 9, type: "assistance", mg: "chest", name: "Tricep Pushdown"},
+      {id: 10, type: "assistance", mg: "back", name: "DB Bicep Curl"}
+    ],
+    athleticGoal: [
+      {choice: "nothin", setsMain: 0, setsAssistance: 0, repsMain: 0, repsAssistance: 0}
+    ],
+    workoutType: [
+      {choice: "nothin"}
+    ],
+    exercisesSelected: [
+      {id: 1, type: "compound", name: "name"}
+    ]
+  }
   render() {
     return (
       <React.Fragment>
